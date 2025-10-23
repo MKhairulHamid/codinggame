39d9b5d9d173a4f790fc7151081229de0cd9ce82
khairul <muhammad.khairul@liven.com.au>
2025-10-23 17:03:35 +0700

    Remove dynamic export from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next';
 import ClientLayout from '@/components/ClientLayout';
 import './globals.css';
 
-export const dynamic = 'force-dynamic';
-
 export const metadata: Metadata = {
   title: 'LTU HTML Generator',
   description: 'Generate HTML5 code for MOODLE LMS',
